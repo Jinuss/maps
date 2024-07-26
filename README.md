b9b39250e7a28a64ecd55e15c41729fc3cfdae29
sunqi <sunqi@leador.com.cn>
2024-07-26 17:54:18 +0800

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+- 二维地图
+  基于 openlayers
+- 三维地图
+  基于 Cesium
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+
+onMounted(() => {
+  const viewer = new Cesium.Viewer("cesium");
+});
+</script>
+
+<template>
+  <div id="cesium"></div>
+</template>
+
+<style scoped>
+#cesium {
+  height: 100%;
+  width: 100%;
+}
+</style>
