8012d550834fb8167ee029e13c720cd9e2332b97
sunqi <zqbysq@163.com>
2024-08-17 15:50:11 +0800

    update

--- src/components/cesium/CesiumMap.vue
+++ src/components/cesium/CesiumMap.vue
@@ -1,35 +1,21 @@
 <script setup lang="ts">
 import { ref, onMounted } from "vue";
+import { CESIUM_VIEWER_OPTIONS, ACCESS_TOKEN } from "../../const/const.cesium";
 
 const Viewer = ref();
 
-onMounted(async () => {
-  const token ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI3ODZkMDQzOS03ZGJjLTQzZWUtYjlmYy04ZmM5Y2UwNzNhMmYiLCJpZCI6MjU5LCJpYXQiOjE2MzgyMDYwMDB9.cK1hsaFBgz0l2dG9Ry5vBFHWp-HF2lwjLC0tcK8Z8tY"
-  Cesium.Ion.defaultAccessToken = token;
+onMounted(
+  () => {
+    Cesium.Ion.defaultAccessToken = ACCESS_TOKEN;
 
-  const viewer = new Cesium.Viewer("cesium", {
-    timeline: false, // 不显示时间线
-    animation: false, // 不显示动画控制
-    geocoder: false, // 不显示搜索按钮编码器
-    homeButton: true, // 显示初视角按钮
-    sceneModePicker: false, // 显示投影方式选择器
-    baseLayerPicker: false, // 显示基础图层选择器
-    navigationHelpButton: false, // 不显示帮助按钮
-    fullscreenButton: true, // 显示全屏按钮,
-    terrain: Cesium.Terrain.fromWorldTerrain(),
+    const viewer = new Cesium.Viewer("cesium", {
+      ...CESIUM_VIEWER_OPTIONS,
+    });
+
+    viewer.cesiumWidget.creditContainer.style.display = "none";
+
+    Viewer.value = viewer;
   });
-  // 隐藏Cesium Ion图标
-  viewer.cesiumWidget.creditContainer.style.display = "none";
-
-  Viewer.value = viewer;
-  try {
-    const tileset = await Cesium.Cesium3DTileset.fromIonAssetId(40866);
-    viewer.scene.primitives.add(tileset);
-    viewer.zoomTo(tileset);
-  } catch (error) {
-    console.log(`Error loading tileset: ${error}`);
-  }
-});
 </script>
 
 <template>
---
+++ src/const/const.cesium.tsx
@@ -0,0 +1,12 @@
+export const CESIUM_VIEWER_OPTIONS = {
+  timeline: false, // 不显示时间线
+  animation: false, // 不显示动画控制
+  geocoder: false, // 不显示搜索按钮编码器
+  homeButton: true, // 显示初视角按钮
+  sceneModePicker: false, // 显示投影方式选择器
+  baseLayerPicker: false, // 显示基础图层选择器
+  navigationHelpButton: false, // 不显示帮助按钮
+  fullscreenButton: true, // 显示全屏按钮,
+};
+
+export const ACCESS_TOKEN="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJlMDU5YzU2NC01NmIxLTQ3ZDYtYjcwYi1kMzQ3ZTVhZTIyODAiLCJpZCI6MjM1MzAwLCJpYXQiOjE3MjM4ODAzNzF9.en0CDXqgdgysiUZ2qtmyR0CmQg3nMXp4k9q9ZuUMdhc"
